577097b4812168587ecd60ba5def1a647f649d9e
YW0916 <YW0916@YW-01B-0523.corp.yaowan.com>
2017-08-02 10:07:37 +0800

    优化代码

--- src/main/java/com/logic/server/Bootstarp.java
+++ src/main/java/com/logic/server/Bootstarp.java
@@ -1,14 +1,21 @@
 package com.logic.server;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 import com.framework.utils.MessageUtil;
 import com.message.MessageMsg.UserInfoMsg_23001;
 
+@SpringBootApplication
+@ComponentScan(basePackages={"com"})  
 public class Bootstarp {
 
 	public static void main(String[] args) {
+		SpringApplication.run(Bootstarp.class, args);
 		UserInfoMsg_23001.Builder userInfo = UserInfoMsg_23001.newBuilder();
 		userInfo.setId(1);
-		userInfo.setName("tom");
+		userInfo.setName("server 1 send to server 2");
 		for(int i=0;i<100;i++){
 			MessageUtil.sendToServer(userInfo.build(), "2");
 			try {
